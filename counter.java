3e02c253156c91104045a853657d1e37007ae782
Wong <bs1019023804@163.com>
2021-04-10 09:36:22 +0800

    Update counter.java

--- counter.java
+++ counter.java
@@ -18,7 +18,7 @@ public class counter extends JFrame implements ActionListener {
         result.setEditable(false);
         add(result);
         int x = 20, y = 55;
-        for (int i = 0; i < keys.length; i++)//放置按钮
+        for (int i = 0; i < keys.length; i++)// 放置按钮
         {
             key[i] = new JButton();
             key[i].setText(keys[i]);
@@ -36,12 +36,14 @@ public class counter extends JFrame implements ActionListener {
     }
 
     private JTextField result = new JTextField();
-    private String[] keys = {"7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", "/", "c", ".", "=", "(", ")", "AC"};
+    private String[] keys = { "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", "/", "c", ".", "=", "(",
+            ")", "AC" };
     private JButton key[] = new JButton[keys.length];
     private Stack<Character> optr = new Stack<Character>();
     private Stack<Double> opnd = new Stack<Double>();
-    private Stack<String> kuohao=new Stack<String>();
-    private boolean pointlock=true;
+    private Stack<String> kuohao = new Stack<String>();
+    private boolean pointlock = true;
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
@@ -59,24 +61,20 @@ public class counter extends JFrame implements ActionListener {
         } else if (Pattern.matches("[-*/+]", label)) {
             if (Pattern.matches("[\\d()]", last_value)) {
                 result.setText(value + label);
-                pointlock=true;
+                pointlock = true;
             }
-        }
-        else if(label=="("&&!Pattern.matches("[()]",last_value)){
-            result.setText(value+label);
+        } else if (label == "(" && !Pattern.matches("[()]", last_value)) {
+            result.setText(value + label);
             kuohao.push(label);
-        }
-        else if(label=="."&&Pattern.matches("\\d",last_value)&&pointlock){
+        } else if (label == "." && Pattern.matches("\\d", last_value) && pointlock) {
 
-                result.setText(value+label);
-                pointlock=false;
-        }
-        else if (label==")"&&!Pattern.matches("[()]",last_value)&&!kuohao.empty()){
+            result.setText(value + label);
+            pointlock = false;
+        } else if (label == ")" && !Pattern.matches("[()]", last_value) && !kuohao.empty()) {
 
-            result.setText(value+label);
+            result.setText(value + label);
             kuohao.pop();
-        }
-        else if (label == "AC")
+        } else if (label == "AC")
             result.setText("");
         else if (label == "c") {
             if (result.getText().length() != 0) {
@@ -85,12 +83,11 @@ public class counter extends JFrame implements ActionListener {
 
             }
         } else if (label == "=") {
-            if(kuohao.empty()){
-            result.setText(value + label);
-            count(result.getText());
-            }
-            else
-                JOptionPane.showMessageDialog(null,"计算式有错","错误",JOptionPane.WARNING_MESSAGE);
+            if (kuohao.empty()) {
+                result.setText(value + label);
+                count(result.getText());
+            } else
+                JOptionPane.showMessageDialog(null, "计算式有错", "错误", JOptionPane.WARNING_MESSAGE);
 
         }
     }
@@ -105,7 +102,6 @@ public class counter extends JFrame implements ActionListener {
         while (!optr.empty()) {
             String p = String.valueOf(c[i]);
 
-
             if (Pattern.matches("[0-9.]", p)) {
                 String n = String.valueOf(c[i + 1]);
                 x = x + p;
@@ -116,31 +112,28 @@ public class counter extends JFrame implements ActionListener {
 
                 }
                 i++;
-            }
-            else {
+            } else {
                 char item = optr.peek();
                 switch (precede(item, c[i])) {
-                    case -1:
-                        optr.push(c[i]);
+                case -1:
+                    optr.push(c[i]);
+                    i++;
+                    break;
+                case 0:
+                    item = optr.pop();
+                    if (item != '=')
                         i++;
-                        break;
-                    case 0:
-                        item = optr.pop();
-                        if (item!= '=')
-                            i++;
-                        break;
-                    case 1:
-                        char theta = optr.pop();
-                        Double b = opnd.pop();
-                        Double a = opnd.pop();
+                    break;
+                case 1:
+                    char theta = optr.pop();
+                    Double b = opnd.pop();
+                    Double a = opnd.pop();
 
-                        opnd.push(opearate(a, theta, b));
-
+                    opnd.push(opearate(a, theta, b));
 
                 }
 
             }
-
 
         }
 
@@ -148,67 +141,93 @@ public class counter extends JFrame implements ActionListener {
         result.setText(String.valueOf(num));
     }
 
-
-
-
-
-    private int inStack(char i){
-        int in=0;
-        switch (i){
-            case '=': in=0;break;
-            case '(':in=1;break;
-            case '*':in=5;break;
-            case '/':in=5;break;
-            case '+':in=3;break;
-            case ')':in=6;break;
-            case '-':in=3;break;
+    private int inStack(char i) {
+        int in = 0;
+        switch (i) {
+        case '=':
+            in = 0;
+            break;
+        case '(':
+            in = 1;
+            break;
+        case '*':
+            in = 5;
+            break;
+        case '/':
+            in = 5;
+            break;
+        case '+':
+            in = 3;
+            break;
+        case ')':
+            in = 6;
+            break;
+        case '-':
+            in = 3;
+            break;
         }
         return in;
     }
-    private int outStack(char i){
-        int out=0;
-        switch (i){
-            case '=': out=0;break;
-            case '(':out=6;break;
-            case '*':out=4;break;
-            case '/':out=4;break;
-            case '+':out=2;break;
-            case ')':out=1;break;
-            case '-':out=2;break;
+
+    private int outStack(char i) {
+        int out = 0;
+        switch (i) {
+        case '=':
+            out = 0;
+            break;
+        case '(':
+            out = 6;
+            break;
+        case '*':
+            out = 4;
+            break;
+        case '/':
+            out = 4;
+            break;
+        case '+':
+            out = 2;
+            break;
+        case ')':
+            out = 1;
+            break;
+        case '-':
+            out = 2;
+            break;
         }
         return out;
     }
-    private int precede(char i,char b){
-        if(inStack(i)<outStack(b)) return -1;
-        else if(inStack(i)==outStack(b)) return 0;
-        else return 1;
-    }
-    private double opearate(double a,char p,double b){
-        double re=0;
-        switch (p) {
-            case '+':
-                re = a + b;
-                break;
-            case '-':
-                re = a - b;
-                break;
-            case '*':
-                re = a * b;
-                break;
-            case '/':
-                re = a / b;
 
-                break;
+    private int precede(char i, char b) {
+        if (inStack(i) < outStack(b))
+            return -1;
+        else if (inStack(i) == outStack(b))
+            return 0;
+        else
+            return 1;
+    }
+
+    private double opearate(double a, char p, double b) {
+        double re = 0;
+        switch (p) {
+        case '+':
+            re = a + b;
+            break;
+        case '-':
+            re = a - b;
+            break;
+        case '*':
+            re = a * b;
+            break;
+        case '/':
+            re = a / b;
+
+            break;
         }
         return re;
     }
 
+    public static void main(String[] args) {
+        new counter();
+    }
+
 }
-
-
-
-
-
-
-
-
