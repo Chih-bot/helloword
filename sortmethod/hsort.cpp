edfbee87b28cf915207c8bad43e370ae5e2bb897
Wong <bs1019023804@163.com>
2021-04-29 16:47:58 +0800

    Update hsort.cpp

--- sortmethod/hsort.cpp
+++ sortmethod/hsort.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 //堆排序
-
  using namespace std;
 int parent(int i){
     return i-1/2;
@@ -35,13 +34,17 @@ void heap(int *a,int i,int length){
 }
 //最大堆的建成,时间复杂度为O(n)
 void bmheap(int *a,int length){
+    //自底向上维护其他节点最大堆的性质
 	for(int i=(length-1)/2;i>=0;i--)//在索引length/2，length/2+1...n的节点为叶节点
 	heap(a,i,length);
 	}
 //堆排序,时间复杂度为O(nlgn)
     void heapsort(int *a,int length){
-	bmheap(a,length);
-	int size=length;
+	bmheap(a,length);//先建成最大堆
+	int size=length;//获取堆的大小
+    /*最大的节点总在第一个,自底向上进行循环，使最其放到正确的位置,然后去掉节点
+    (通过size-1),然后在维护根节点最大堆的性质
+    */
 	for(int i=length-1;i>=1;i--){
 		int tmp=a[i];
 		a[i]=a[0];
