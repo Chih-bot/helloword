93a51ce114568d7924334285321280a0fa6d412d
Wong <bs1019023804@163.com>
2021-04-30 00:33:11 +0800

    归并排序
    加入了注释

--- sortmethod/mergesort.cpp
+++ sortmethod/mergesort.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
+/*
+归并排序，采用分治的思想，将要排序数组分为各个小的数组，
+比较排序，时间复杂度为Θ(nlgn),递归式为T(n)=2T(n/2)+T(n)
+*/
 void merge(int *a,int p,int q,int r){
-    int n1=q-p;
-    int n2=r-q;
+    int n1=q-p;//获得左子数组的长度
+    int n2=r-q;//获得右子数组的长度
     int *l=new int [n1];
     int *d=new int [n2];
-    for(int i=0;i<n1;i++)
+    for(int i=0;i<n1;i++)//传入数据
     l[i]=a[p+i];
-    for(int i=0;i<n2;i++)
+    for(int i=0;i<n2;i++)//传入数据
     d[i]=a[q+i];
-    int i=0;
-    int j=0;
-    int k=p;
+    int i=0;//左子数组的索引
+    int j=0;//右字数组的索引
+    int k=p;//排序数组的索引
+    
+    //通过比较左右两个数组的值,得出排序数组当前索引的值
     while(i<n1&&j<n2){
         if(l[i]<=d[j]){
             a[k]=l[i];
@@ -37,12 +43,13 @@ void merge(int *a,int p,int q,int r){
     delete []l;
     delete []d;
 }
+//归并排序，递归方法
 void mergesort(int *a,int p,int r){
     if(p<r){
-        int q=(p+r)/2;
-        mergesort(a,p,q);
-        mergesort(a,q+1,r);
-       merge(a,p,q,r);
+        int q=(p+r)/2;//划分为2
+        mergesort(a,p,q);//左划分
+        mergesort(a,q+1,r);//右划分
+       merge(a,p,q,r);//进行排序
     }
 }
 int main(){
