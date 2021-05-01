6c371b64c1741cb5f002f8e1722caed45a98750f
Wong <bs1019023804@163.com>
2021-05-01 11:56:11 +0800

    快速排序
    增加了随机化版本

--- sortmethod/quicksort.cpp
+++ sortmethod/quicksort.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<stdlib.h>
+
 /*快速排序,选择一个主元，把比这元素小的放左边，
 大的放右边，然后移动其主元的位置，最后划分出两
 个子数组
@@ -9,7 +11,7 @@ int partition(int *a,int p,int r){
     int q=p-1;//主元将要换入的位置
     for(int j=p;j<r;j++){
          
-         if(a[j]<x){
+         if(a[j]<=x){
              q++;
              int tmp=a[j];
              a[j]=a[q];
@@ -32,12 +34,31 @@ void quicksort(int *a,int p,int r){
         quicksort(a,q+1,r);
     }
 }
+/*
+随机化版本
+*/
+int randompartition(int *a,int p,int r){
+    int i=rand()%(r-p+1)+p;
+    int tmp=a[i];
+    a[i]=a[r];
+    a[r]=tmp;
+    return partition(a,p,r);
+}
+void randomquciksort(int *a,int p,int r){
+    if(p<r){
+        int q=randompartition(a,p,r);
+        randomquciksort(a,p,q);
+        randomquciksort(a,q+1,r);
+    }
+}
 
 int main(){
     using namespace std;
     int a[5]={1,3,2,5,4};
     int r=sizeof(a)/sizeof(a[0])-1;
-    quicksort(a,0,r);
-    for(int i=0;i<=r;i++)
-    cout<<a[i]<<"\t";
+    randomquciksort(a,0,r);
+    for(int i=0;i<=r;i++){
+        cout<<a[i]<<"\t";
+    }
+
 }
